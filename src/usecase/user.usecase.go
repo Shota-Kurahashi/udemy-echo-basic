0293bf94ecb04a75b6379a40a2a46d68bb241e93
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-06-19 20:46:33 +0900

    add validator

--- src/usecase/user.usecase.go
+++ src/usecase/user.usecase.go
@@ -4,6 +4,7 @@ import (
 	"ge-rest-api/src/model"
 	"ge-rest-api/src/repository"
 	"ge-rest-api/src/utils"
+	"ge-rest-api/src/validator"
 )
 
 type IUserUseCase interface {
@@ -13,13 +14,19 @@ type IUserUseCase interface {
 
 type userUseCase struct {
 	ur repository.IUserRepository
+	uv validator.IUserValidator
 }
 
-func NewUserUseCase(ur repository.IUserRepository) IUserUseCase {
-	return &userUseCase{ur}
+func NewUserUseCase(ur repository.IUserRepository, uv validator.IUserValidator) IUserUseCase {
+	return &userUseCase{ur, uv}
 }
 
 func (uu *userUseCase) SignUp(user model.User) (model.UserResponse, error) {
+
+	if err := uu.uv.UserValidate(user); err != nil {
+		return model.UserResponse{}, err
+	}
+
 	hashed, err := utils.HashPassword(user.Password)
 
 	if err != nil {
@@ -38,6 +45,11 @@ func (uu *userUseCase) SignUp(user model.User) (model.UserResponse, error) {
 }
 
 func (uu *userUseCase) Login(user model.User) (string, error) {
+
+	if err := uu.uv.UserValidate(user); err != nil {
+		return "", err
+	}
+
 	storedUser := model.User{}
 
 	if err := uu.ur.GetUserByEmail(&storedUser, user.Email); err != nil {
